17802a2f4bef9ca816fa13b37cd89aae7e5e93d1
rto24 <richardto180@gmail.com>
2025-01-10 16:32:28 -0500

    rendered user line chart to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import DataCards from "@/components/DataCards";
+import TotalActiveUsersChart from "@/components/TotalActiveUsers";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center m-4">
       <h1 className="text-4xl font-bold">Streamify Dashboard</h1>
       <DataCards />
+      <TotalActiveUsersChart />
     </div>
   );
 }
