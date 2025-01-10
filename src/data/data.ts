4cce1d6db15032222b5b1eca5dd321c1bcbbeaa5
rto24 <richardto180@gmail.com>
2025-01-10 15:16:21 -0500

    aggregated card data in data file

---
+++ src/data/data.ts
@@ -0,0 +1,28 @@
+export const dataCardMatrics = {
+  totalUsers: {
+    title: "Total Users",
+    description: "The total number of registered users on Streamify",
+    value: "2,123,903",
+  },
+  activeUsers: {
+    title: "Active Users",
+    description: "The number of users who have streamed at least one songs in the last 30 days",
+    value: "1,809,202",
+  },
+  totalStreams: {
+    title: "Total Streams",
+    description: "The total number of streams on the platform",
+    value: "563,780,204",
+  },
+  totalRevenue: {
+    title: "Total Revenue",
+    description: "The total revenue generated from subscriptions and advertisements",
+    value: "$20,083,826"
+  },
+  topArtist: {
+    title: "Taylor Swift",
+    description: "The artist with the most streams this month",
+    value: "1,029,590,291"
+  }
+}
+
